2c34f433430c963aebda94ad327aecb9926546a6
Quockm <mjnhquoc21101997@gmail.com>
2020-09-02 13:53:54 +0700

    Re-design Navigation Drawer, Add Sign out menu & Display user information

--- app/src/main/java/com/example/doancna/Common.java
+++ app/src/main/java/com/example/doancna/Common.java
@@ -3,9 +3,19 @@ package com.example.doancna;
 import com.example.doancna.Model.DriverInfoModel;
 
 public class Common {
-    public static final String DRIVER_INFO_REFERENCE = "DriverInfo" ;
+    public static final String DRIVER_INFO_REFERENCE = "DriverInfo";
     public static final String DRIVER_LOCATION_REFERENCE = "DriverLocation";
 
     public static DriverInfoModel currentUser;
 
+    public static String buildWelcomeMessage() {
+        if (Common.currentUser != null) {
+            return new StringBuilder("Welcome ")
+                    .append(Common.currentUser.getFisrtnasme())
+                    .append(" ")
+                    .append(Common.currentUser.getLastname()).toString();
+        } else {
+            return "";
+        }
+    }
 }
--- app/src/main/java/com/example/doancna/DriverHomeActivity.java
+++ app/src/main/java/com/example/doancna/DriverHomeActivity.java
@@ -1,13 +1,21 @@
 package com.example.doancna;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -19,6 +27,10 @@ import androidx.appcompat.widget.Toolbar;
 public class DriverHomeActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +44,62 @@ public class DriverHomeActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.nav_sign_out) {
+                    Context context;
+                    AlertDialog.Builder builder = new AlertDialog.Builder(DriverHomeActivity.this);
+                    builder.setTitle("Sign Out")
+                            .setMessage("Do you want to sign out!!")
+                            .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.dismiss();
+                                }
+                            })
+                            .setPositiveButton("SIGN OUT", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    FirebaseAuth.getInstance().signOut();
+                                    Intent intent = new Intent(DriverHomeActivity.this, SplashScreenActivity.class);
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            })
+                            .setCancelable(false);
+                    AlertDialog dialog = builder.create();
+                    dialog.setOnShowListener(dialogInterface -> {
+                        dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+                                .setTextColor(getResources().getColor(android.R.color.holo_red_dark));
+                        dialog.getButton(AlertDialog.BUTTON_NEGATIVE)
+                                .setTextColor(getResources().getColor(R.color.colorAccent));
+
+                    });
+                    dialog.show();
+                }
+                return true;
+            }
+        });
+
+        // set data for user
+        View headerView = navigationView.getHeaderView(0);
+        TextView txt_name = (TextView) headerView.findViewById(R.id.txt_name);
+        TextView txt_phonenumber = (TextView) headerView.findViewById(R.id.txt_phonenumber);
+        TextView txt_star = (TextView) headerView.findViewById(R.id.txt_star);
+
+        txt_name.setText(Common.buildWelcomeMessage());
+        txt_phonenumber.setText(Common.currentUser != null ? Common.currentUser.getPhonenumber() : "");
+        txt_star.setText(Common.currentUser != null ? String.valueOf(Common.currentUser.getRating()) : "0.0");
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
